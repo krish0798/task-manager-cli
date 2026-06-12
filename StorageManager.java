f3d21ec25ad5533a31c25c83b7a294c427b7d860
Krish07 <krishjaiswar778@gmail.com>
2026-06-12 17:55:46 +0530

    initial structure: Task, TaskManager, StorageManager, Main classes

---
+++ StorageManager.java
@@ -0,0 +1,3 @@
+class StorageManager {
+    String filename = "tasks.json";
+}
---
+++ Task.java
@@ -0,0 +1,7 @@
+class Task {
+    int id;
+    String description;
+    String status;
+    String createdAt;
+    String updatedAt;
+}
